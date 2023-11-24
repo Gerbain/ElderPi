3a2ad4b3edbafbc025397d4449240152ebb6a9c6
Gerbain <gerbgeer@gmail.com>
2023-11-24 14:33:13 +0100

    fix walking

--- player.py
+++ player.py
@@ -9,16 +9,44 @@ class Player(pygame.sprite.Sprite):
         self.current_sprite = self.sprites['idle'][0]
         self.rect = self.current_sprite.get_rect(center=pos)
         self.speed = CHARACTER_SPEED
+        self.direction = pygame.math.Vector2(0, 0)
 
     def load_sprites(self, sprite_sheet):
-        # Load the sprites here and return a dictionary of animations
-        # Each animation is a list of frames, e.g., {'idle': [frame1, frame2, ...], 'walk': [frame1, frame2, ...]}
-        pass
+        animations = {
+            'idle': [],
+            'walk': []
+        }
+
+        for row, anim in enumerate(animations.keys()):
+            for col in range(4):  # Assuming 4 frames per animation
+                frame = sprite_sheet.subsurface(pygame.Rect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                animations[anim].append(frame)
+
+        return animations
 
     def update(self, keys):
-        # Update the player's position based on keys pressed
-        pass
+        self.direction.x, self.direction.y = 0, 0  # Reset movement direction
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.direction.x = 1
+
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.direction.y = 1
+
+        # Normalize direction vector to ensure consistent speed diagonally
+        if self.direction.length() > 0:
+            self.direction = self.direction.normalize()
+
+        # Update position
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+        # Update animation
+        # You can add logic here to switch between 'idle' and 'walk' animations based on movement
 
     def draw(self, screen):
-        # Draw the player
         screen.blit(self.current_sprite, self.rect.topleft)
